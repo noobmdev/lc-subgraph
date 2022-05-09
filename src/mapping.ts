68d3242d268c0bbbc85afe5459755916405f16e4
noobmdev <noobmdev@gmail.com>
2022-05-09 17:59:17 +0700

    update graph handler

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,5 +1,6 @@
 import { Submission as SubmissionEvent } from "../generated/Precondition/Precondition";
-import { Submission } from "../generated/schema";
+import { NewStandardLC as NewStandardLCEvent } from "../generated/LC/StandardLCFactory";
+import { Submission, StandardLC } from "../generated/schema";
 
 export function handleSubmission(event: SubmissionEvent): void {
   let entity = new Submission(
@@ -8,5 +9,15 @@ export function handleSubmission(event: SubmissionEvent): void {
   entity.documentId = event.params.documentId;
   entity.caller = event.params.caller;
   entity.time = event.params.time;
+  entity.save();
+}
+
+export function handleNewStandardLC(event: NewStandardLCEvent): void {
+  let entity = new StandardLC(
+    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
+  );
+  entity.documentId = event.params.documentID;
+  entity.creator = event.params.creator;
+  entity.address = event.params.lcContractAddr;
   entity.save();
 }
