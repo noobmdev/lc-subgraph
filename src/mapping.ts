43cf8d0d9651dfdd12a6cbd8240914ad23f1f881
noobmdev <noobmdev@gmail.com>
2022-05-10 14:53:23 +0700

    update graph handler

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,23 +1,27 @@
-import { Submission as SubmissionEvent } from "../generated/Precondition/Precondition";
 import { NewStandardLC as NewStandardLCEvent } from "../generated/LC/StandardLCFactory";
-import { Submission, StandardLC } from "../generated/schema";
-
-export function handleSubmission(event: SubmissionEvent): void {
-  let entity = new Submission(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  );
-  entity.documentId = event.params.documentId;
-  entity.caller = event.params.caller;
-  entity.time = event.params.time;
-  entity.save();
-}
+import { Approved as ApprovedEvent } from "../generated/templates/StageContract/StageContract";
+import { StandardLC, LCApproved } from "../generated/schema";
+import { StageContract } from "../generated/templates";
 
 export function handleNewStandardLC(event: NewStandardLCEvent): void {
-  let entity = new StandardLC(
+  let standardLC = new StandardLC(event.params.lcContractAddr);
+  standardLC.documentId = event.params.documentID;
+  standardLC.creator = event.params.creator;
+  standardLC.save();
+
+  // create the tracked contract based on the template
+  StageContract.create(event.params.lcContractAddr);
+}
+
+export function handleApproved(event: ApprovedEvent): void {
+  let lcApproved = new LCApproved(
     event.transaction.hash.toHex() + "-" + event.logIndex.toString()
   );
-  entity.documentId = event.params.documentID;
-  entity.creator = event.params.creator;
-  entity.address = event.params.lcContractAddr;
-  entity.save();
+  lcApproved.lc = event.address;
+  lcApproved.organization = event.params.organization;
+  lcApproved.stage = event.params.stage;
+  lcApproved.subStage = event.params.subStage;
+  lcApproved.documentId = event.params.documentID;
+  lcApproved.approvedTime = event.params.approvedTime;
+  lcApproved.save();
 }
